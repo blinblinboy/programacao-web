84215519150a59f338d62f0e0fdc09bd5959cf81
rhuan <rhuan.cm@sempreceub.com>
2024-03-21 09:21:29 -0300

    aulaa

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let var1 = 5;
 let var2 = 30;
 
-console.log(var1 + var2);
+let resultado1 = var1 + var2;
+
+//console.log(resultado1);
+//console.log(typeof(resultado1);
+
+let variavelArray = [5, 4, 8, 0, -7, "abc", 8.3];
+
+console.log(variavelArray[0]);
