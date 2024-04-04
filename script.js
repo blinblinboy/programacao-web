a4e81d4c262519d54590ca6db29484eb6ed6cd03
rhuan <rhuan.cm@sempreceub.com>
2024-04-04 10:48:35 -0300

    conversosChicoMoedas

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ let resultado1 = var1 + var2;
 let variavelArray = [5, 4, 8, 0, -7, "abc", 8.3];
 
 console.log(variavelArray[0]);
+
+let variavelObjeto = {
+    nome: "Mariana",
+    idade: 16,
+    altura: 1.65,
+    cpf: 139371031
+}
+
+console.log(variavelObjeto.nome);
